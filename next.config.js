e2703a6e3360fe7b754a350972b2446ae986c7c5
PlopesK <popi081004@hotmail.com>
2023-08-23 20:27:03 -0300

    Adjusts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   env: {
     API_URL: "https://64a54c6300c3559aa9bf7245.mockapi.io",
     DOMAIN_ORIGIN: "http://localhost:3000",
--- src/app/api/heroes/route.ts
+++ src/app/api/heroes/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import JSONData from "./heroes.json";
 
 export async function GET() {
-  let res = await fetch(`${process.env.API_URL}/api/heroes`);
+  let res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
   let data = await res.json();
 
   if (!data) {
